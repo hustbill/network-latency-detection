6f5182d819eb5cb2c971e62f4a598b0c9e2fec9b
hua zhang <hua.zhang1@huawwei.com>
2017-05-23 10:08:49 -0700

    add data into influxdb

---
+++ src/adddata.sh
@@ -0,0 +1,21 @@
+
+# round 1 data
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node3",remotenode="k8s-node3"  value=0.09'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node2",remotenode="k8s-node3"  value=0.88'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node2",remotenode="k8s-node2"  value=0.08'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node1",remotenode="k8s-node1"  value=0.12'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node1",remotenode="k8s-node2"  value=0.81'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node1",remotenode="k8s-node3"  value=0.97'
+
+
+# round 2 data
+
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node3",remotenode="k8s-node3"  value=0.12'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node2",remotenode="k8s-node3"  value=0.94'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node2",remotenode="k8s-node2"  value=0.08'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node1",remotenode="k8s-node1"  value=0.10'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node1",remotenode="k8s-node2"  value=90.75'
+curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelatency,localnode="k8s-node1",remotenode="k8s-node3"  value=0.76'
+
+
+
