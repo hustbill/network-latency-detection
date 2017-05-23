efff8d43f5162432e220abd580c6ff163bd56761
Hua Zhang <hua.zhang1@huawei.com>
2017-05-23 10:42:57 -0700

    k8s-nodes display

--- src/createnode.sh
+++ src/createnode.sh
@@ -1,5 +1,5 @@
 
-curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelist,name="k8s-node1"  value=1'
-curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelist,name="k8s-node2"  value=1'
-curl -i -XPOST 'http://192.168.254.134:8086/write?db=ican' --data-binary 'nodelist,name="k8s-node3"  value=1'
+curl -i -XPOST 'http://localhost:8086/write?db=ican' --data-binary 'nodelist,name="k8s-node1"  value=1'
+curl -i -XPOST 'http://localhost:8086/write?db=ican' --data-binary 'nodelist,name="k8s-node2"  value=1'
+curl -i -XPOST 'http://localhost:8086/write?db=ican' --data-binary 'nodelist,name="k8s-node3"  value=1'
 
