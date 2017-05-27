d5d5d196a3ea906d82c1b7006b3e98493bb3f51d
Hua Zhang <hua.zhang1@huawei.com>
2017-05-26 17:03:05 -0700

    debug fo cytoscape

--- js/csv.js
+++ js/csv.js
@@ -1,11 +1,11 @@
 /**
- * csv.js 
+ * csv.js
  */
 
 var prices_csv = function()
 {
     var parse = d3.time.format("%m/%d/%Y").parse;
-    d3.csv("prices.csv", function(prices)
+    d3.csv("../data/prices.csv", function(prices)
     {
         //prices is an array of json objects containing the data in from the csv
         console.log("prices:", prices)
@@ -16,7 +16,7 @@ var prices_csv = function()
             month = parse(d.month).getMonth();
             console.log("month:", d.month, month)
             //we slice the dollar sign off then convert to a number with the + sign
-            //slicing works like "$216".slice(1) gives you 216, 
+            //slicing works like "$216".slice(1) gives you 216,
             //you can also give it a range like "$216 asdf".slice(1,4) gives you 216
             p = d.price
             price = +p.slice(1)
@@ -30,7 +30,7 @@ var prices_csv = function()
 var trans_csv = function()
 {
     var parse = d3.time.format("%m/%d/%Y").parse;
-    d3.csv("trans.csv", function(trans)
+    d3.csv("../data/trans.csv", function(trans)
     {
         //prices is an array of json objects containing the data in from the csv
         console.log("trans:", trans)
